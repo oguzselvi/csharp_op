75903702ab6e06e469e9e5ee4e22de24d6bb4026
oguz selvi <oslc98@gmail.com>
2023-02-06 13:14:13 +0300

    yeni konu tamamlandı

---
+++ Program.cs
@@ -0,0 +1,58 @@
+namespace second;
+class Program
+{
+    static void Main(string[] args)
+    {
+        int x = 3;
+        int y = 4;
+        y = y + 2 ;
+        
+        Console.WriteLine(y);
+        y/=2;
+        Console.WriteLine(y);
+        y/=1;
+        Console.WriteLine(y);
+        x*=2;
+        Console.WriteLine(x);
+
+        bool isSucces = true ;
+        bool isCompleted = false ;
+
+        if(isSucces && isCompleted)
+            Console.WriteLine("Perfect!");
+
+        if(isSucces || isCompleted) 
+            Console.Write("Great!");
+
+        if(isSucces && !isCompleted) 
+            Console.WriteLine("Fine!");
+            int a = 3 ;
+            int b = 4 ;//
+            bool sonuc = a<b ;
+            Console.WriteLine(sonuc);
+            sonuc = a>b ;  
+            Console.WriteLine(sonuc);
+            sonuc = a>=b ;
+            Console.WriteLine(sonuc);
+            sonuc = a<=b ;
+            Console.WriteLine(sonuc);
+            sonuc = a==b ;
+            Console.WriteLine(sonuc);
+            sonuc = a!=b ;
+            Console.WriteLine(sonuc);
+
+            int sayi1   = 10 ; 
+            int sayi2   = 5 ;
+            int sonuc1  =  sayi1/sayi2 ; 
+            Console.WriteLine(sonuc1);
+            sonuc1 = sayi1 * sayi2;
+            Console.WriteLine(sonuc1);
+            sonuc1 = sayi1 + sayi2;
+            Console.WriteLine(sonuc1);
+            sonuc1 = sayi1+=1 ;
+            Console.WriteLine(sonuc1);
+            
+            int sonuc2 = 20%3 ;
+            Console.WriteLine(sonuc2);
+    }
+}
